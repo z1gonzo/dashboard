9eadc56c3774990897c5b6d258e32571f6d8db82
z1gonzo <goonsky@gmail.com>
2022-07-17 08:40:49 +0200

    Navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,9 +19,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       className="relative text-xl rounded-full p-3 hover:bg-light-gray"
     >
       <span style={{ background: dotColor }}
-        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-        {icon}
-      </span>
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" />
+      {icon}
     </button>
 
   </TooltipComponent>
@@ -29,8 +28,23 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 
 function Navbar() {
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext()
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext()
 
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth)
+
+    window.addEventListener('resize', handleResize)
+
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true)
+    }
+  }, [screenSize])
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
